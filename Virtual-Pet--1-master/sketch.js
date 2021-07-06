f133bb0d773f207f19a98f41b39d8bd18de17b11
Aniket08Coder <78540230+Aniket08Coder@users.noreply.github.com>
2021-07-06 09:32:13 +0530

    Add files via upload

---
+++ Virtual-Pet--1-master/sketch.js
@@ -0,0 +1,24 @@
+//Create variables here
+var dog, happyDog, foodS, foodStock;
+var dogImag, happyDogImage
+function preload()
+{
+	dogImag = loadImage(images/doglmg.png);
+  happyDogImage = loadImage(images/doglmg1.png);
+}
+
+function setup() {
+	createCanvas(800, 700);
+  
+}
+
+
+function draw() {  
+
+  drawSprites();
+  //add styles here
+
+}
+
+
+
